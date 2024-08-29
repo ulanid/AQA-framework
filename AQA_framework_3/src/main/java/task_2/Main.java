f3cf9e1febb52c3da4fe9190cc6c7a0187729a42
ulanid <116159219+ulanid@users.noreply.github.com>
2024-08-29 22:14:50 +0300

    init

---
+++ AQA_framework_3/src/main/java/task_2/Main.java
@@ -0,0 +1,18 @@
+package task_2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int v1 = 10;
+        int v2 = 20;
+
+        if (v1 > v2) {
+            System.out.println("Variable 'v1' is greater than 'v2'");
+        } else if (v1 < v2) {
+            System.out.println("Variable 'v2' is greater than 'v1'");
+        } else {
+            System.out.println("Both variables are equal");
+        }
+
+    }
+}
